41a1723ad5d93b7bd8280eda7a611642c05a80a7
Kyle Sunderland <sunderlandkyl@gmail.com>
2022-03-24 16:37:08 -0400

    ENH: Add module to compute a surface between two Freesurfer models

---
+++ SurfaceAverage/SurfaceAverage.py
@@ -0,0 +1,327 @@
+import os
+import unittest
+import logging
+import vtk, qt, ctk, slicer
+from slicer.ScriptedLoadableModule import *
+from slicer.util import VTKObservationMixin
+import numpy as np
+
+#
+# SurfaceAverage
+#
+
+class SurfaceAverage(ScriptedLoadableModule):
+  """Uses ScriptedLoadableModule base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def __init__(self, parent):
+    ScriptedLoadableModule.__init__(self, parent)
+    self.parent.title = "SurfaceAverage"  # TODO: make this more human readable by adding spaces
+    self.parent.categories = ["Examples"]  # TODO: set categories (folders where the module shows up in the module selector)
+    self.parent.dependencies = []  # TODO: add here list of module names that this module requires
+    self.parent.contributors = ["John Doe (AnyWare Corp.)"]  # TODO: replace with "Firstname Lastname (Organization)"
+    # TODO: update with short description of the module and a link to online module documentation
+    self.parent.helpText = """
+This is an example of scripted loadable module bundled in an extension.
+See more information in <a href="https://github.com/organization/projectname#SurfaceAverage">module documentation</a>.
+"""
+    # TODO: replace with organization, grant and thanks
+    self.parent.acknowledgementText = """
+This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc., Andras Lasso, PerkLab,
+and Steve Pieper, Isomics, Inc. and was partially funded by NIH grant 3P41RR013218-12S1.
+"""
+
+#
+# SurfaceAverageWidget
+#
+
+class SurfaceAverageWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
+  """Uses ScriptedLoadableModuleWidget base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def __init__(self, parent=None):
+    """
+    Called when the user opens the module the first time and the widget is initialized.
+    """
+    ScriptedLoadableModuleWidget.__init__(self, parent)
+    VTKObservationMixin.__init__(self)  # needed for parameter node observation
+    self.logic = None
+    self._parameterNode = None
+    self._updatingGUIFromParameterNode = False
+
+  def setup(self):
+    """
+    Called when the user opens the module the first time and the widget is initialized.
+    """
+    ScriptedLoadableModuleWidget.setup(self)
+
+    # Load widget from .ui file (created by Qt Designer).
+    # Additional widgets can be instantiated manually and added to self.layout.
+    uiWidget = slicer.util.loadUI(self.resourcePath('UI/SurfaceAverage.ui'))
+    self.layout.addWidget(uiWidget)
+    self.ui = slicer.util.childWidgetVariables(uiWidget)
+
+    # Set scene in MRML widgets. Make sure that in Qt designer the top-level qMRMLWidget's
+    # "mrmlSceneChanged(vtkMRMLScene*)" signal in is connected to each MRML widget's.
+    # "setMRMLScene(vtkMRMLScene*)" slot.
+    uiWidget.setMRMLScene(slicer.mrmlScene)
+
+    # Create logic class. Logic implements all computations that should be possible to run
+    # in batch mode, without a graphical user interface.
+    self.logic = SurfaceAverageLogic()
+
+    # Connections
+
+    # These connections ensure that we update parameter node when scene is closed
+    self.addObserver(slicer.mrmlScene, slicer.mrmlScene.StartCloseEvent, self.onSceneStartClose)
+    self.addObserver(slicer.mrmlScene, slicer.mrmlScene.EndCloseEvent, self.onSceneEndClose)
+
+    # These connections ensure that whenever user changes some settings on the GUI, that is saved in the MRML scene
+    # (in the selected parameter node).
+    self.ui.inputModel1Selector.connect("currentNodeChanged(vtkMRMLNode*)", self.updateParameterNodeFromGUI)
+    self.ui.inputModel2Selector.connect("currentNodeChanged(vtkMRMLNode*)", self.updateParameterNodeFromGUI)
+    self.ui.outputModelSelector.connect("valueChanged(double)", self.updateParameterNodeFromGUI)
+
+    # Buttons
+    self.ui.applyButton.connect('clicked(bool)', self.onApplyButton)
+
+    # Make sure parameter node is initialized (needed for module reload)
+    self.initializeParameterNode()
+
+  def cleanup(self):
+    """
+    Called when the application closes and the module widget is destroyed.
+    """
+    self.removeObservers()
+
+  def enter(self):
+    """
+    Called each time the user opens this module.
+    """
+    # Make sure parameter node exists and observed
+    self.initializeParameterNode()
+
+  def exit(self):
+    """
+    Called each time the user opens a different module.
+    """
+    # Do not react to parameter node changes (GUI wlil be updated when the user enters into the module)
+    self.removeObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self.updateGUIFromParameterNode)
+
+  def onSceneStartClose(self, caller, event):
+    """
+    Called just before the scene is closed.
+    """
+    # Parameter node will be reset, do not use it anymore
+    self.setParameterNode(None)
+
+  def onSceneEndClose(self, caller, event):
+    """
+    Called just after the scene is closed.
+    """
+    # If this module is shown while the scene is closed then recreate a new parameter node immediately
+    if self.parent.isEntered:
+      self.initializeParameterNode()
+
+  def initializeParameterNode(self):
+    """
+    Ensure parameter node exists and observed.
+    """
+    # Parameter node stores all user choices in parameter values, node selections, etc.
+    # so that when the scene is saved and reloaded, these settings are restored.
+
+    self.setParameterNode(self.logic.getParameterNode())
+
+    # Select default input nodes if nothing is selected yet to save a few clicks for the user
+    if not self._parameterNode.GetNodeReference("InputVolume"):
+      firstVolumeNode = slicer.mrmlScene.GetFirstNodeByClass("vtkMRMLScalarVolumeNode")
+      if firstVolumeNode:
+        self._parameterNode.SetNodeReferenceID("InputVolume", firstVolumeNode.GetID())
+
+  def setParameterNode(self, inputParameterNode):
+    """
+    Set and observe parameter node.
+    Observation is needed because when the parameter node is changed then the GUI must be updated immediately.
+    """
+
+    if inputParameterNode:
+      self.logic.setDefaultParameters(inputParameterNode)
+
+    # Unobserve previously selected parameter node and add an observer to the newly selected.
+    # Changes of parameter node are observed so that whenever parameters are changed by a script or any other module
+    # those are reflected immediately in the GUI.
+    if self._parameterNode is not None:
+      self.removeObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self.updateGUIFromParameterNode)
+    self._parameterNode = inputParameterNode
+    if self._parameterNode is not None:
+      self.addObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self.updateGUIFromParameterNode)
+
+    # Initial GUI update
+    self.updateGUIFromParameterNode()
+
+  def updateGUIFromParameterNode(self, caller=None, event=None):
+    """
+    This method is called whenever parameter node is changed.
+    The module GUI is updated to show the current state of the parameter node.
+    """
+
+    if self._parameterNode is None or self._updatingGUIFromParameterNode:
+      return
+
+    # Make sure GUI changes do not call updateParameterNodeFromGUI (it could cause infinite loop)
+    self._updatingGUIFromParameterNode = True
+
+    # Update node selectors and sliders
+    inputModel1 = self._parameterNode.GetNodeReference("InputModel1")
+    inputModel2 = self._parameterNode.GetNodeReference("InputModel2")
+    outputModel = self._parameterNode.GetNodeReference("OutputModel")
+
+    self.ui.inputModel1Selector.setCurrentNode(inputModel1)
+    self.ui.inputModel2Selector.setCurrentNode(inputModel2)
+    self.ui.outputModelSelector.setCurrentNode(outputModel)
+
+    # Update buttons states and tooltips
+    if inputModel1 and inputModel2 and outputModel and inputModel1.GetPolyData().GetNumberOfPoints() == inputModel2.GetPolyData().GetNumberOfPoints():
+      self.ui.applyButton.toolTip = "Compute output model"
+      self.ui.applyButton.enabled = True
+    else:
+      self.ui.applyButton.toolTip = "Select input and output model nodes. The number of points in the input models must be the same"
+      self.ui.applyButton.enabled = False
+
+    # All the GUI updates are done
+    self._updatingGUIFromParameterNode = False
+
+  def updateParameterNodeFromGUI(self, caller=None, event=None):
+    """
+    This method is called when the user makes any change in the GUI.
+    The changes are saved into the parameter node (so that they are restored when the scene is saved and loaded).
+    """
+
+    if self._parameterNode is None or self._updatingGUIFromParameterNode:
+      return
+
+    wasModified = self._parameterNode.StartModify()  # Modify all properties in a single batch
+
+    self._parameterNode.SetNodeReferenceID("InputModel1", self.ui.inputModel1Selector.currentNodeID)
+    self._parameterNode.SetNodeReferenceID("InputModel2", self.ui.inputModel2Selector.currentNodeID)
+    self._parameterNode.SetNodeReferenceID("OutputModel", self.ui.outputModelSelector.currentNodeID)
+
+    self._parameterNode.EndModify(wasModified)
+
+  def onApplyButton(self):
+    """
+    Run processing when user clicks "Apply" button.
+    """
+    with slicer.util.tryWithErrorDisplay("Failed to compute results.", waitCursor=True):
+      inputModel1 = self._parameterNode.GetNodeReference("InputModel1")
+      inputModel2 = self._parameterNode.GetNodeReference("InputModel2")
+      outputModel = self._parameterNode.GetNodeReference("OutputModel")
+
+      # Compute output
+      self.logic.process(inputModel1, inputModel2, outputModel)
+
+#
+# SurfaceAverageLogic
+#
+
+class SurfaceAverageLogic(ScriptedLoadableModuleLogic):
+  """This class should implement all the actual
+  computation done by your module.  The interface
+  should be such that other python code can import
+  this class and make use of the functionality without
+  requiring an instance of the Widget.
+  Uses ScriptedLoadableModuleLogic base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def __init__(self):
+    """
+    Called when the logic class is instantiated. Can be used for initializing member variables.
+    """
+    ScriptedLoadableModuleLogic.__init__(self)
+
+  def setDefaultParameters(self, parameterNode):
+    """
+    Initialize parameter node with default settings.
+    """
+    pass
+
+  def process(self, inputModel1, inputModel2, outputModel):
+    """
+    Run the processing algorithm.
+    Can be used without GUI widget.
+    :param inputModel1: Input model to be averaged
+    :param inputModel2: Input model to be averaged
+    :param outputModel: Result model
+    """
+
+    if not inputModel1 or not inputModel2 or not outputModel:
+      raise ValueError("Input or output model is invalid")
+
+    import time
+    startTime = time.time()
+    logging.info('Processing started')
+
+    outputPolyData = vtk.vtkPolyData()
+    outputPolyData.DeepCopy(inputModel1.GetPolyData())
+
+    inputModel1Points = inputModel1.GetPolyData().GetPoints()
+    inputModel2Points = inputModel2.GetPolyData().GetPoints()
+    outputPoints = outputPolyData.GetPoints()
+
+    numberOfPoints = outputPoints.GetNumberOfPoints()
+    for i in range(numberOfPoints):
+      model1Point = np.array(inputModel1Points.GetPoint(i))
+      model2Point = np.array(inputModel2Points.GetPoint(i))
+      outputModelPoint = (model1Point + model2Point)/2.0
+      outputPoints.SetPoint(i, outputModelPoint)
+      if i == 0:
+        print(model1Point)
+        print(model2Point)
+        print(outputModelPoint)
+    outputPoints.Modified()
+    outputModel.Modified()
+    outputModel.SetAndObservePolyData(outputPolyData)
+
+    stopTime = time.time()
+    logging.info(f'Processing completed in {stopTime-startTime:.2f} seconds')
+
+#
+# SurfaceAverageTest
+#
+
+class SurfaceAverageTest(ScriptedLoadableModuleTest):
+  """
+  This is the test case for your scripted module.
+  Uses ScriptedLoadableModuleTest base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def setUp(self):
+    """ Do whatever is needed to reset the state - typically a scene clear will be enough.
+    """
+    slicer.mrmlScene.Clear()
+
+  def runTest(self):
+    """Run as few or as many tests as needed here.
+    """
+    self.setUp()
+    self.test_SurfaceAverage1()
+
+  def test_SurfaceAverage1(self):
+    """ Ideally you should have several levels of tests.  At the lowest level
+    tests should exercise the functionality of the logic with different inputs
+    (both valid and invalid).  At higher levels your tests should emulate the
+    way the user would interact with your code and confirm that it still works
+    the way you intended.
+    One of the most important features of the tests is that it should alert other
+    developers when their changes will have an impact on the behavior of your
+    module.  For example, if a developer removes a feature that you depend on,
+    your test should break so they know that the feature is needed.
+    """
+
+    self.delayDisplay("Starting the test")
+
+    self.delayDisplay('Test passed')
