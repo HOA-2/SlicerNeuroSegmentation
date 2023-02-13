5c38236d24034841765b5e4c348bd429699fe476
Kyle Sunderland <sunderlandkyl@gmail.com>
2023-02-13 11:58:31 -0500

    ENH: Add editor effect for copying segments from other Segmentations

---
+++ NeuroSegmentEffects/SegmentEditorNeuroSegmentEffects.py
@@ -0,0 +1,139 @@
+import os
+
+import vtk
+
+import slicer
+from slicer.ScriptedLoadableModule import *
+
+
+class SegmentEditorNeuroSegmentEffects(ScriptedLoadableModule):
+    """Uses ScriptedLoadableModule base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def __init__(self, parent):
+        ScriptedLoadableModule.__init__(self, parent)
+        self.parent.title = "SegmentEditorNeuroSegmentEffects"
+        self.parent.categories = ["Segmentation"]
+        self.parent.dependencies = ["Segmentations"]
+        self.parent.contributors = ["Kyle Sunderland (PerkLab)"]
+        self.parent.hidden = True
+        self.parent.helpText = "This hidden module registers the segment editor effect"
+        self.parent.helpText += self.getDefaultModuleDocumentationLink()
+        self.parent.acknowledgementText = "Supported by NA-MIC, NAC, BIRN, NCIGT, and the Slicer Community. See https://www.slicer.org for details."
+        slicer.app.connect("startupCompleted()", self.registerEditorEffect)
+
+    def registerEditorEffect(self):
+        import qSlicerSegmentationsEditorEffectsPythonQt as qSlicerSegmentationsEditorEffects
+        moveInstance = qSlicerSegmentationsEditorEffects.qSlicerSegmentEditorScriptedEffect(None)
+        moveEffectFilename = os.path.join(os.path.dirname(__file__), self.__class__.__name__ + 'Lib/MoveSegmentEditorEffect.py')
+        moveInstance.setPythonSource(moveEffectFilename.replace('\\', '/'))
+        moveInstance.self().register()
+
+
+class SegmentEditorNeuroSegmentEffectsTest(ScriptedLoadableModuleTest):
+    """
+    This is the test case for your scripted module.
+    Uses ScriptedLoadableModuleTest base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def setUp(self):
+        """ Do whatever is needed to reset the state - typically a scene clear will be enough.
+        """
+        slicer.mrmlScene.Clear(0)
+
+    def runTest(self):
+        """Run as few or as many tests as needed here.
+        """
+        self.setUp()
+        self.test_NeuroSegmentEffects1()
+
+    def test_NeuroSegmentEffects1(self):
+        """
+        Basic automated test of the segmentation method:
+        - Create segmentation by placing sphere-shaped seeds
+        - Run segmentation
+        - Verify results using segment statistics
+        The test can be executed from SelfTests module (test name: SegmentEditorNeuroSegmentEffects)
+        """
+
+        self.delayDisplay("Starting test_NeuroSegmentEffects1")
+
+        import vtkSegmentationCorePython as vtkSegmentationCore
+        import SampleData
+        from SegmentStatistics import SegmentStatisticsLogic
+
+        ##################################
+        self.delayDisplay("Load source volume")
+
+        sourceVolumeNode = SampleData.downloadSample('MRBrainTumor1')
+
+        ##################################
+        self.delayDisplay("Create segmentation containing a few spheres")
+
+        segmentationNode = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLSegmentationNode')
+        segmentationNode.CreateDefaultDisplayNodes()
+        segmentationNode.SetReferenceImageGeometryParameterFromVolumeNode(sourceVolumeNode)
+
+        # Segments are defined by a list of: name and a list of sphere [radius, posX, posY, posZ]
+        segmentGeometries = [
+            ['Tumor', [[10, -6, 30, 28]]],
+            ['Background', [[10, 0, 65, 22], [15, 1, -14, 30], [12, 0, 28, -7], [5, 0, 30, 54], [12, 31, 33, 27], [17, -42, 30, 27], [6, -2, -17, 71]]],
+            ['Air', [[10, 76, 73, 0], [15, -70, 74, 0]]]]
+        for segmentGeometry in segmentGeometries:
+            segmentName = segmentGeometry[0]
+            appender = vtk.vtkAppendPolyData()
+            for sphere in segmentGeometry[1]:
+                sphereSource = vtk.vtkSphereSource()
+                sphereSource.SetRadius(sphere[0])
+                sphereSource.SetCenter(sphere[1], sphere[2], sphere[3])
+                appender.AddInputConnection(sphereSource.GetOutputPort())
+            segment = vtkSegmentationCore.vtkSegment()
+            segment.SetName(segmentationNode.GetSegmentation().GenerateUniqueSegmentID(segmentName))
+            appender.Update()
+            segment.AddRepresentation(vtkSegmentationCore.vtkSegmentationConverter.GetSegmentationClosedSurfaceRepresentationName(), appender.GetOutput())
+            segmentationNode.GetSegmentation().AddSegment(segment)
+
+        ##################################
+        self.delayDisplay("Create segment editor")
+
+        segmentEditorWidget = slicer.qMRMLSegmentEditorWidget()
+        segmentEditorWidget.show()
+        segmentEditorWidget.setMRMLScene(slicer.mrmlScene)
+        segmentEditorNode = slicer.vtkMRMLSegmentEditorNode()
+        slicer.mrmlScene.AddNode(segmentEditorNode)
+        segmentEditorWidget.setMRMLSegmentEditorNode(segmentEditorNode)
+        segmentEditorWidget.setSegmentationNode(segmentationNode)
+        segmentEditorWidget.setSourceVolumeNode(sourceVolumeNode)
+
+        ##################################
+        self.delayDisplay("Run segmentation")
+        segmentEditorWidget.setActiveEffectByName("NeuroSegmentEffects")
+        effect = segmentEditorWidget.activeEffect()
+        effect.setParameter("ObjectScaleMm", 3.0)
+        effect.self().onApply()
+
+        ##################################
+        self.delayDisplay("Make segmentation results nicely visible in 3D")
+        segmentationDisplayNode = segmentationNode.GetDisplayNode()
+        segmentationDisplayNode.SetSegmentVisibility("Air", False)
+        segmentationDisplayNode.SetSegmentOpacity3D("Background", 0.5)
+
+        ##################################
+        self.delayDisplay("Compute statistics")
+
+        segStatLogic = SegmentStatisticsLogic()
+        segStatLogic.computeStatistics(segmentationNode, sourceVolumeNode)
+
+        # Export results to table (just to see all results)
+        resultsTableNode = slicer.vtkMRMLTableNode()
+        slicer.mrmlScene.AddNode(resultsTableNode)
+        segStatLogic.exportToTable(resultsTableNode)
+        segStatLogic.showTable(resultsTableNode)
+
+        self.delayDisplay("Check a few numerical results")
+        self.assertEqual(round(segStatLogic.statistics["Tumor", "LM volume cc"]), 16)
+        self.assertEqual(round(segStatLogic.statistics["Background", "LM volume cc"]), 3010)
+
+        self.delayDisplay('test_NeuroSegmentEffects1 passed')
---
+++ NeuroSegmentEffects/SegmentEditorNeuroSegmentEffectsLib/MoveSegmentEditorEffect.py
@@ -0,0 +1,89 @@
+import logging
+import os
+
+import qt
+import vtk
+
+import slicer
+
+from SegmentEditorEffects import *
+
+
+class MoveSegmentEditorEffect(AbstractScriptedSegmentEditorEffect):
+    """This effect uses Watershed algorithm to partition the input volume"""
+
+    def __init__(self, scriptedEffect):
+        scriptedEffect.name = 'Move segments'
+        scriptedEffect.perSegment = True
+        scriptedEffect.requireSegments = True  # this effect requires segment(s) existing in the segmentation
+        AbstractScriptedSegmentEditorEffect.__init__(self, scriptedEffect)
+
+    def clone(self):
+        # It should not be necessary to modify this method
+        import qSlicerSegmentationsEditorEffectsPythonQt as effects
+        clonedEffect = effects.qSlicerSegmentEditorScriptedEffect(None)
+        clonedEffect.setPythonSource(__file__.replace('\\', '/'))
+        return clonedEffect
+
+    def icon(self):
+        # It should not be necessary to modify this method
+        iconPath = os.path.join(os.path.dirname(__file__), 'MoveSegmentEditorEffect.png')
+        if os.path.exists(iconPath):
+            return qt.QIcon(iconPath)
+        return qt.QIcon()
+
+    def helpText(self):
+        return """Move a segment from another segmentation into the selected segment."""
+
+    def setupOptionsFrame(self):
+
+        # Object scale slider
+        self.segmentSelectorWidget = slicer.qMRMLSegmentSelectorWidget()
+        self.segmentSelectorWidget.setToolTip('Choose the segment to copy the segment from.')
+        self.segmentSelectorWidget.setMRMLScene(slicer.mrmlScene)
+        self.scriptedEffect.addLabeledOptionsWidget("Source segment:", self.segmentSelectorWidget)
+
+        # Apply button
+        self.applyButton = qt.QPushButton("Apply")
+        self.applyButton.objectName = self.__class__.__name__ + 'Apply'
+        self.applyButton.setToolTip("Move segment from the source to the destination segment.")
+        self.scriptedEffect.addOptionsWidget(self.applyButton)
+        self.applyButton.connect('clicked()', self.onApply)
+
+    def createCursor(self, widget):
+        # Turn off effect-specific cursor for this effect
+        return slicer.util.mainWindow().cursor
+
+    def setMRMLDefaults(self):
+        pass
+
+    def updateGUIFromMRML(self):
+        pass
+
+    def updateMRMLFromGUI(self):
+        pass
+
+    def onApply(self):
+
+        # This can be a long operation - indicate it to the user
+        qt.QApplication.setOverrideCursor(qt.Qt.WaitCursor)
+
+        # Allow users revert to this state by clicking Undo
+        self.scriptedEffect.saveStateForUndo()
+
+        modifierLabelmap = self.scriptedEffect.defaultModifierLabelmap()
+        originalImageToWorldMatrix = vtk.vtkMatrix4x4()
+        modifierLabelmap.GetImageToWorldMatrix(originalImageToWorldMatrix)
+
+        sourceSegmentation = self.segmentSelectorWidget.currentNode()
+        sourceSegmentID = self.segmentSelectorWidget.currentSegmentID()
+
+        sourceLabelmap = slicer.vtkOrientedImageData()
+        sourceSegmentation.GetBinaryLabelmapRepresentation(sourceSegmentID, sourceLabelmap)
+
+        slicer.vtkOrientedImageDataResample.ResampleOrientedImageToReferenceOrientedImage(sourceLabelmap, modifierLabelmap, modifierLabelmap)
+
+        # Apply changes
+        self.scriptedEffect.modifySelectedSegmentByLabelmap(modifierLabelmap, slicer.qSlicerSegmentEditorAbstractEffect.ModificationModeSet)
+
+        qt.QApplication.restoreOverrideCursor()
